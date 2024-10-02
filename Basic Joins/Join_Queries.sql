f6a08c1b882ee2aa6acf7e43271e494009f91c61
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:32:52 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -4,10 +4,7 @@
 
 
 -- Problem #1068: Join Sales and Product to retrieve product name, year, and price
-SELECT P.product_name, S.year, S.price 
-FROM Sales AS S
-JOIN Product AS P 
-  ON S.product_id = P.product_id;
+
 
 -- Problem #1581: Count number of visits without a transaction per customer
 SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans 
