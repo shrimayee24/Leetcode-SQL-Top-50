66f68dc969a73079f12b46c523d49736fd80405c
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:39:12 +0530

    Update 1661.sql

--- Basic Joins/1661.sql
+++ Basic Joins/1661.sql
@@ -1,3 +1,4 @@
+--Problem #1661: Average Time of Process per Machine
 SELECT s.machine_id,round(avg(e.timestamp-s.timestamp),3) AS processing_time
 FROM Activity s JOIN Activity e ON
     s.machine_id = e.machine_id AND s.process_id = e.process_id AND
