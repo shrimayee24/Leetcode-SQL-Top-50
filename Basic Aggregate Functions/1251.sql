c1934d7936db2c78c84f2f722ade951116149fdb
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:31:41 +0530

    Create 1251.sql

---
+++ Basic Aggregate Functions/1251.sql
@@ -0,0 +1,4 @@
+SELECT P.product_id,round(COALESCE(SUM(P.price *U.units)/SUM(U.units),0),2) as average_price 
+FROM Prices P LEFT JOIN UnitsSold U ON P.product_id=U.product_id
+AND U.purchase_date BETWEEN P.start_date AND P.end_date 
+GROUP BY P.product_id
