89f2142cf2f4b7c1e012f0816c18a3e846f8d1b1
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:35:16 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -13,11 +13,7 @@
 
 
 --Problem #1661: Average Time of Process per Machine
-SELECT s.machine_id,round(avg(e.timestamp-s.timestamp),3) AS processing_time
-FROM Activity s JOIN Activity e ON
-    s.machine_id = e.machine_id AND s.process_id = e.process_id AND
-    s.activity_type = 'start' AND e.activity_type = 'end'
-    GROUP BY s.machine_id
+
 
 --Problem #577: Employee Bonus
 # Write your MySQL query statement below
