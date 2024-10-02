b16d3758185ec9b1b87f1049ae95c3956ef362cc
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:15:16 +0530

    Update Function_queries.sql

--- Basic Aggregate Functions/Function_queries.sql
+++ Basic Aggregate Functions/Function_queries.sql
@@ -3,3 +3,9 @@
 --Problem #620: Not Boring Movies
 SELECT id,movie,description,rating FROM Cinema WHERE id%2=1 AND description!='boring'
 ORDER BY rating DESC
+
+--Problem #1251: Average Selling Price
+SELECT P.product_id,round(COALESCE(SUM(P.price *U.units)/SUM(U.units),0),2) as average_price 
+FROM Prices P LEFT JOIN UnitsSold U ON P.product_id=U.product_id
+AND U.purchase_date BETWEEN P.start_date AND P.end_date 
+GROUP BY P.product_id
