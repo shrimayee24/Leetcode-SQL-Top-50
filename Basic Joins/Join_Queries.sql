cc1a29880bfc55b0e3509eb14c883d0eab41ed39
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:35:54 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -15,10 +15,5 @@
 --Problem #1661: Average Time of Process per Machine
 
 
---Problem #577: Employee Bonus
-# Write your MySQL query statement below
-SELECT e.name,b.bonus 
-FROM Employee e
-LEFT JOIN Bonus b ON e.empId=b.empId 
-WHERE b.bonus<1000 OR b.bonus IS NULL
+
 
