19ddd6b2a953b23582462d23bcc8e1a1c31b2d33
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:33:33 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -7,12 +7,7 @@
 
 
 -- Problem #1581: Count number of visits without a transaction per customer
-SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans 
-FROM Visits v
-LEFT JOIN Transactions t 
-  ON t.visit_id = v.visit_id 
-WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id;
+
 
 -- Problem #197: Find weather records where temperature is higher than the previous day
 SELECT w1.id 
