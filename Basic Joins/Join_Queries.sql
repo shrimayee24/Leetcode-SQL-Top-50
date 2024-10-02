14ec9d7a461ad64c95f6d84d68070dc6c87acf50
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:32:05 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -1,10 +1,7 @@
 -- 2.Basic Joins
 
 -- Problem #1378: Left join Employees with EmployeeUNI on their id
-SELECT EU.unique_id, E.name 
-FROM Employees E
-LEFT JOIN EmployeeUNI EU 
-  ON EU.id = E.id;
+
 
 -- Problem #1068: Join Sales and Product to retrieve product name, year, and price
 SELECT P.product_name, S.year, S.price 
