a4f0d564c84038264b2c717a4cf85c514552b93d
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:36:43 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -1,18 +1,3 @@
--- 2.Basic Joins
-
--- Problem #1378: Left join Employees with EmployeeUNI on their id
-
-
--- Problem #1068: Join Sales and Product to retrieve product name, year, and price
-
-
--- Problem #1581: Count number of visits without a transaction per customer
-
-
--- Problem #197: Find weather records where temperature is higher than the previous day
-
-
---Problem #1661: Average Time of Process per Machine
 
 
 
