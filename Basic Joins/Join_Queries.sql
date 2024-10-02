e608bd9ca8a729334922a511da82c426bfcbe1ee
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 00:52:10 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -27,4 +27,10 @@ JOIN Weather w2
   ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
 WHERE w1.temperature > w2.temperature;
 
+--Problem #1661: Average Time of Process per Machine
+SELECT s.machine_id,round(avg(e.timestamp-s.timestamp),3) AS processing_time
+FROM Activity s JOIN Activity e ON
+    s.machine_id = e.machine_id AND s.process_id = e.process_id AND
+    s.activity_type = 'start' AND e.activity_type = 'end'
+    GROUP BY s.machine_id
 
