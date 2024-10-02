98071760edf5899e1a209c201f39caecff16ce77
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:34:45 +0530

    Update Join_Queries.sql

--- Basic Joins/Join_Queries.sql
+++ Basic Joins/Join_Queries.sql
@@ -10,11 +10,7 @@
 
 
 -- Problem #197: Find weather records where temperature is higher than the previous day
-SELECT w1.id 
-FROM Weather w1
-JOIN Weather w2 
-  ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
-WHERE w1.temperature > w2.temperature;
+
 
 --Problem #1661: Average Time of Process per Machine
 SELECT s.machine_id,round(avg(e.timestamp-s.timestamp),3) AS processing_time
