168cab3e3796b61628f51b33dde1c071cf8bcd83
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:31:23 +0530

    Update Function_queries.sql

--- Basic Aggregate Functions/Function_queries.sql
+++ Basic Aggregate Functions/Function_queries.sql
@@ -4,7 +4,4 @@
 
 
 --Problem #1251: Average Selling Price
-SELECT P.product_id,round(COALESCE(SUM(P.price *U.units)/SUM(U.units),0),2) as average_price 
-FROM Prices P LEFT JOIN UnitsSold U ON P.product_id=U.product_id
-AND U.purchase_date BETWEEN P.start_date AND P.end_date 
-GROUP BY P.product_id
+
