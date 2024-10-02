59653a7c948e0b332898ff2dad96d02341d7f8a6
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-03 02:30:54 +0530

    Update Function_queries.sql

--- Basic Aggregate Functions/Function_queries.sql
+++ Basic Aggregate Functions/Function_queries.sql
@@ -1,8 +1,7 @@
 --Basic Aggregate Functions
 
 --Problem #620: Not Boring Movies
-SELECT id,movie,description,rating FROM Cinema WHERE id%2=1 AND description!='boring'
-ORDER BY rating DESC
+
 
 --Problem #1251: Average Selling Price
 SELECT P.product_id,round(COALESCE(SUM(P.price *U.units)/SUM(U.units),0),2) as average_price 
