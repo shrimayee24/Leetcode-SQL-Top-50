81b28308466acde4a3eb2a89f6d89ceb1c278506
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-13 17:09:30 +0530

    Create 1484.sql

---
+++ Advanced String Functions+Regex+Clause/1484.sql
@@ -0,0 +1,5 @@
+#denormalization
+SELECT sell_date,COUNT(DISTINCT product) AS num_sold,
+GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
