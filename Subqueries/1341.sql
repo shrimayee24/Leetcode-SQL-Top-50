1a5bb7d396dc39d28b3d0d833d84da0d9ae1080d
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-11-05 14:23:07 +0530

    Update 1341.sql

--- Subqueries/1341.sql
+++ Subqueries/1341.sql
@@ -5,44 +5,44 @@
 #create name record
 (
     #map user ids in movie rating table to names in users table
-    select
-        Users.name as results
-    from 
-        MovieRating left join Users
-    on 
+    SELECT
+        Users.name AS results
+    FROM
+        MovieRating LEFT JOIN Users
+   ON
         MovieRating.user_id = Users.user_id 
     #group duplicate names
-    group by
+    GROUP BY
         Users.user_id
     #order using criteria- highest number of movie rating:count(movie id) and dictionary order (Users.name)
-    order by
-        count(MovieRating.movie_id) desc, 
+    ORDER BY
+        COUNT(MovieRating.movie_id) DESC, 
         Users.name
     #select 1st record of sorted records
-    limit 1
+    LIMIT 1
 ) 
 #join 2 records
-union all
+UNION ALL
 
 #create record for movie
 (
     #map movie titles to movie ids
-    select 
-        Movies.title as results
-    from 
-        MovieRating  left join Movies 
-    on
+   SELECT
+        Movies.title AS results
+    FROM
+        MovieRating  LEFT JOIN Movies 
+    ON
         MovieRating.movie_id = Movies.movie_id
     #use like operator to select movies in February 2020
-    where 
-        MovieRating.created_at like '2020-02%'
+    WHERE
+        MovieRating.created_at LIKE '2020-02%'
     #group duplicate titles
-    group by
+    GROUP BY
         MovieRating.movie_id
     #order using criteria- highest average of movie rating:count(movie id) and dictionary order (movies.title)
-    order by
-        avg(MovieRating.rating) desc, 
+    ORDER BY
+        avg(MovieRating.rating) DESC, 
         Movies.title
      #select 1st record of sorted records
-    limit 1
+   LIMIT 1
 )
